b42f0ca6e6c27fc3672ec316fd366de348543459
Russkazkin <russkazkin@gmail.com>
2021-11-26 05:04:32 +0300

    Add some html

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const ExpenseItem = () => {
-  return <h2>Expense item!</h2>;
+  return (
+    <>
+      <div>May 29th 2021</div>
+      <h2>Car Insurance</h2>
+      <div>$256.78</div>
+    </>
+  );
 };
 
 export default ExpenseItem;
