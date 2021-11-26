d57b2b4e39489274cb8a55d3d749cdf4fabb79d3
Russkazkin <russkazkin@gmail.com>
2021-11-26 04:55:35 +0300

    Add ExpenseItem componetn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ExpenseItem from './Components/ExpenseItem';
 
 const App = () => {
   return (
     <div>
       <h2>Let&apos;s get started!</h2>
+      <ExpenseItem />
     </div>
   );
 };
