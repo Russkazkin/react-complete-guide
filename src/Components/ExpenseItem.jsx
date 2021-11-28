c6a07dd8a23e3686996a8cfda5f7873033c9dcad
Russkazkin <russkazkin@gmail.com>
2021-11-28 10:34:19 +0300

    Add styles for ExpenseItem component

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import './ExpenseItem.sass';
 
 const ExpenseItem = () => {
   return (
-    <>
+    <div className="expense-item">
       <div>May 29th 2021</div>
-      <h2>Car Insurance</h2>
-      <div>$256.78</div>
-    </>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$256.78</div>
+      </div>
+    </div>
   );
 };
 
