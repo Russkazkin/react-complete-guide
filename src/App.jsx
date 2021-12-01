595fe2b9a07e123b551d81483710cb36c67abcbc
Russkazkin <russkazkin@gmail.com>
2021-12-01 05:54:39 +0300

    Add human readable date

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,15 @@ import { uniqueId } from 'lodash';
 const App = () => {
   const expenses = [
     { title: 'Car Insurance', amount: 256.78, date: new Date(2021, 10, 29) },
-    { title: 'Car Insurance', amount: 256.78, date: new Date(2021, 10, 29) },
-    { title: 'Car Insurance', amount: 256.78, date: new Date(2021, 10, 29) },
+    { title: 'Doctor', amount: 500, date: new Date(2021, 10, 29) },
+    { title: 'Food', amount: 16.75, date: new Date(2021, 10, 29) },
   ];
   return (
     <div>
       <h2>Let&apos;s get started!</h2>
       {expenses.map((item) => (
-        <ExpenseItem key={uniqueId('expense')} title={item.title} amount={item.amount} date={item.date.toISOString()} />
+        <ExpenseItem key={uniqueId('expense')} title={item.title} amount={item.amount} date={item.date} />
       ))}
-      <ExpenseItem />
     </div>
   );
 };
--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './ExpenseItem.sass';
 
+/**
+ * @param {String} title
+ * @param {Number} amount
+ * @param {Date} date
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const ExpenseItem = ({ title, amount, date }) => {
+  const month = date.toLocaleString('en-EN', { month: 'long' });
+  const day = date.toLocaleString('en-EN', { day: '2-digit' });
   return (
     <div className="expense-item">
-      <div>{date}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{date.getFullYear()}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
