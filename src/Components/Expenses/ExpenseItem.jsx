0c574e4caa367e75c64f12aa618602d09cb62297
Russkazkin <russkazkin@gmail.com>
2021-12-05 05:45:22 +0300

    Use state to rerender component

--- src/Components/Expenses/ExpenseItem.jsx
+++ src/Components/Expenses/ExpenseItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.sass';
 import ExpenseDate from './ExpenseDate';
 
@@ -10,14 +10,18 @@ import ExpenseDate from './ExpenseDate';
  * @constructor
  */
 const ExpenseItem = ({ title, amount, date }) => {
+  const [expenseTitle, setExpenseTitle] = useState(title);
+  const clickHandler = () => {
+    setExpenseTitle(`New ${title}`);
+  };
   return (
     <>
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={() => console.log('Clicked!')}>Change title</button>
+      <button onClick={clickHandler}>Change title</button>
     </>
   );
 };
