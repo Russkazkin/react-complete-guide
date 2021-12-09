3dd1b391654b399341e0509a1f89b98372579d31
Russkazkin <russkazkin@gmail.com>
2021-12-09 05:25:41 +0300

    Add ExpenseFilter component

--- src/Components/Expenses/Expenses.jsx
+++ src/Components/Expenses/Expenses.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { uniqueId } from 'lodash';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.sass';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 
 const Expenses = ({ expenses }) => {
+  const [filteredYear, setFilteredYear] = useState('2021');
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <>
+      <div>
+        <ExpenseFilter onChangeFilter={filterChangeHandler} year={filteredYear} />
+      </div>
       {expenses.map((item) => (
         <Card className="expense-item" key={uniqueId('expense')}>
           <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
