e7653782d21d0013d2f3db9515a5e6910db32289
Russkazkin <russkazkin@gmail.com>
2021-12-09 05:46:48 +0300

    Remove test buttons from ExpenseItem component

--- src/Components/Expenses/ExpenseItem.jsx
+++ src/Components/Expenses/ExpenseItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ExpenseItem.sass';
 import ExpenseDate from './ExpenseDate';
 
@@ -10,18 +10,13 @@ import ExpenseDate from './ExpenseDate';
  * @constructor
  */
 const ExpenseItem = ({ title, amount, date }) => {
-  const [expenseTitle, setExpenseTitle] = useState(title);
-  const clickHandler = () => {
-    setExpenseTitle(`New ${title}`);
-  };
   return (
     <>
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
     </>
   );
 };
