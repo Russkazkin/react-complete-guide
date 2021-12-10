d45ae1191d5a6dfabe5e6034c1ea4059e56d320e
Russkazkin <russkazkin@gmail.com>
2021-12-10 05:18:47 +0300

    Add expenses array into state

--- src/Components/Expenses/Expenses.jsx
+++ src/Components/Expenses/Expenses.jsx
@@ -16,7 +16,7 @@ const Expenses = ({ expenses }) => {
         <ExpenseFilter onChangeFilter={filterChangeHandler} year={filteredYear} />
       </div>
       {expenses.map((item) => (
-        <ExpenseItem title={item.title} amount={item.amount} date={item.date}  key={uniqueId('expense')} />
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} key={item.id} />
       ))}
     </>
   );
