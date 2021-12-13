02796af19693a6a16bf5338fd006e24253c9d458
Russkazkin <russkazkin@gmail.com>
2021-12-13 05:10:06 +0300

    Add ChartBar component

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import './ChartBar';
-import './Chart.sass';
 import { uniqueId } from 'lodash';
+import ChartBar from './ChartBar';
+import './Chart.sass';
 
 const Chart = ({ dataPoints }) => {
- return (
-   <div className="chart">
-     {dataPoints.map(item => <ChartBar value={item.value} maxValue={null} label={item.label} key={uniqueId('bar')} />)}
-   </div>
- );
+  return (
+    <div className="chart">
+      {dataPoints.map((item) => (
+        <ChartBar value={item.value} maxValue={null} label={item.label} key={uniqueId('bar')} />
+      ))}
+    </div>
+  );
 };
 
 export default Chart;
