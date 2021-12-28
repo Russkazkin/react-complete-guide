151eb6bc1585f2493d264be1712c373f42eac434
Russkazkin <russkazkin@gmail.com>
2021-12-28 06:04:41 +0300

    Merge new project structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import Expenses from './Components/Expenses/Expenses';
 import Card from './Components/UI/Card';
 import NewExpense from './Components/NewExpense/NewExpense';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import MainHeader from './Components/MainHeader/MainHeader';
 
 const DUMMY_EXPENSES = [
   { id: 'e1', title: 'Car Insurance', amount: 256.78, date: new Date(2021, 10, 29) },
@@ -11,17 +14,33 @@ const DUMMY_EXPENSES = [
 ];
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => [expense, ...prevExpenses]);
   };
   return (
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Card className="expenses">
-        <Expenses expenses={expenses} />
-      </Card>
-    </div>
+    <>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        <div>
+          <NewExpense onAddExpense={addExpenseHandler} />
+          <Card className="expenses">
+            <Expenses expenses={expenses} />
+          </Card>
+        </div>
+      </main>
+    </>
   );
 };
 
